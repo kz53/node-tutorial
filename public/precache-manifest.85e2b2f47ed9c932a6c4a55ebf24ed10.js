fe26c3a5ddd29d9f9e81be4a628949c7e0115cbf
Kelley Zhang <kz53@cornell.edu>
2020-12-17 23:55:36 -0500

    civ app test

---
+++ public/precache-manifest.85e2b2f47ed9c932a6c4a55ebf24ed10.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966d128f0105c00957527ef36f3daf96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d3b4f58392438e26582",
+    "url": "/static/css/main.7271efbc.chunk.css"
+  },
+  {
+    "revision": "54d9e005e244cc149174",
+    "url": "/static/js/2.a4ef0c39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4ef0c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3b4f58392438e26582",
+    "url": "/static/js/main.cd229f44.chunk.js"
+  },
+  {
+    "revision": "3fd74cea73a2f44d6669",
+    "url": "/static/js/runtime-main.2258f6a9.js"
+  },
+  {
+    "revision": "44e9da3c4f98af428bfe51897da138b4",
+    "url": "/static/media/ancient-walls.44e9da3c.png"
+  },
+  {
+    "revision": "4807a193ddb89975a1370cce04fd315a",
+    "url": "/static/media/bank.4807a193.png"
+  },
+  {
+    "revision": "55755e17bae1d22675d54c8f5b0ef801",
+    "url": "/static/media/campus.55755e17.png"
+  },
+  {
+    "revision": "76da54a48b3cb66238d8c80d41d9e0e7",
+    "url": "/static/media/city-center.76da54a4.png"
+  },
+  {
+    "revision": "4b25ddb6d8e7d1cd28e62ad79908acdf",
+    "url": "/static/media/commercial-hub.4b25ddb6.png"
+  },
+  {
+    "revision": "2726944df50c473fc9a4a2799f1ccb3a",
+    "url": "/static/media/granary.2726944d.png"
+  },
+  {
+    "revision": "a636c0e052bb439d18a86535374bfc6c",
+    "url": "/static/media/library.a636c0e0.png"
+  },
+  {
+    "revision": "e383d4a6ba06df43c3609f5d3d733749",
+    "url": "/static/media/market.e383d4a6.png"
+  },
+  {
+    "revision": "46c6c2600668245cf1468039f4556a40",
+    "url": "/static/media/monument.46c6c260.png"
+  },
+  {
+    "revision": "ac686f5c8cbed81b1d9d70457ff7c26a",
+    "url": "/static/media/sewer.ac686f5c.png"
+  },
+  {
+    "revision": "71aa4cd58a58b40267a6076f98bea38f",
+    "url": "/static/media/university.71aa4cd5.png"
+  },
+  {
+    "revision": "1714bb54d71ddbdf1f7bb3dc0621fa5a",
+    "url": "/static/media/water-mill.1714bb54.png"
+  }
+]);
